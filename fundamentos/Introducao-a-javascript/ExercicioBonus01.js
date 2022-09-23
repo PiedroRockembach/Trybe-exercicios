2401eae129cb22888a01b4d25517d7e86ea69c90
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-09-23 18:38:28 -0300

    4.3 exBonus 1

---
+++ fundamentos/Introducao-a-javascript/ExercicioBonus01.js
@@ -0,0 +1,11 @@
+let num = 5;
+let sqr = '';
+for(let index2 = 1; index2 <= num; index2 += 1)
+{
+        sqr += '*';
+}
+for (let index = 1; index <= num; index += 1 )
+{
+    
+    console.log(sqr);
+}
